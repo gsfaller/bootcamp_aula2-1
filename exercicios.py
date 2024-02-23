53c6c63f69afeb64223082cc435cf26d18dd33fd
gabriel faller <faller9712@hotmail.com>
2024-02-22 21:26:42 -0300

    salvando exercicios da aula

---
+++ exercicios.py
@@ -0,0 +1,62 @@
+# #### Inteiros (`int`)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+num_1 = int(input("Insira 1 número:"))
+num_2 = int(input("Insira 2 número:"))
+print(num_1 + num_2)
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+num_resto = int(input("Insira um número para ser dividido por 5 e retornar apenas o resto da divisão:"))
+resto_divisao = (num_resto % 5)
+print(f"O resto da sua divisão é: {resto_divisao}")
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+try: 
+    num4 = int(input("Insira um número inteiro, por favor:"))
+    num5 = int(input("Insira um número inteiro, por favor:"))
+    resultado = num4 // num5
+    print(resultado)
+except ZeroDivisionError:
+    print("integer division or modulo by zero")
+except KeyboardInterrupt:
+    print("acho que você interrompeu o programa")
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+# #### Números de Ponto Flutuante (`float`)
+
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+# #### Strings (`str`)
+
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+# #### Booleanos (`bool`)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+# #### try-except e if
+    
+    # ex
+
+# 21: Conversor de Temperatura
+# 22: Verificador de Palíndromo
+# 23: Calculadora Simples
+# 24: Classificador de Números
+# 25: Conversão de Tipo com Validação
