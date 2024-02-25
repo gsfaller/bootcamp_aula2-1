11cccac0c7949b7eca191d4ff6063dc16f9af767
gabriel faller <faller9712@hotmail.com>
2024-02-25 10:00:43 -0300

    atualizaado até exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -75,14 +75,24 @@ resultado_exponencial = numero_base ** exponencial
 
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 
-temp_celsius = input("Insira a temperatura em Celsius:")
+temp_celsius = int(input("Insira a temperatura em Celsius:"))
+fahrenheit = (temp_celsius * 9/5 ) + 32
 
+print(fahrenheit)
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+raio = int(input("Insira o valor do raio"))
+area = 3.14 * (raio ** 2)
+
+print(area)
 
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
